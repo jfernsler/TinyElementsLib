27ef6452aebb7cc286c4045e826c3ead3d387558
Jeremy Fernsler <jfernsler@mac.com>
2023-08-09 16:11:39 -0700

    Update for os agnoastic path needs

--- src/tinyelements/globals.py
+++ src/tinyelements/globals.py
@@ -4,8 +4,16 @@ THIS_DIR = os.path.dirname(os.path.abspath(__file__))
 DEFAULT_THUMB = os.path.join(THIS_DIR, 'resources', 'placeholder.gif')
 
 # Replace these with the paths to your global library of 2D elements, projects, and the structure of your element subpath below the project
-GLOBAL_DIR = '.../_Library/_Elements/_2D/'
-PROJ_DIR = '.../SHOWS'
+# this needs to be constructed in an OS agnostic way, so use os.path.join
+# if you have the follwing:
+# GLOBAL_DIR = '.../_Library/_Elements/_2D/'
+# turn it into this:
+GLOBAL_DIR = os.path.join('...','_Library','_Elements','_2D')
+
+#PROJ_DIR = '.../SHOWS'
+PROJ_DIR = os.path.join('...','SHOWS')
+
+# This subpath defines where elements will be stored under the project when copied in
 SHOW_ELEMENTS_SUBPATH = os.path.join('assets', '_Elements', '_2D')
 
 # Replace this with a call to your environment variables
